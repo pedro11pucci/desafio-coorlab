82bb11610a9f8462965c956003a4e93d37f636b5
pedro11pucci <pedro11pucci@gmail.com>
2024-03-20 18:56:53 -0300

    Create TripOption component

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/fetch')
+def fetch():
+    with open('../data.json') as file:
+        data = json.load(file)
+
+    transports = data['transport']
+    cities = set()
+
+    for transport in transports:
+        city = transport['city']
+        if city not in cities:
+            cities.add(city)
+
+    return jsonify(sorted((list(cities))))
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
