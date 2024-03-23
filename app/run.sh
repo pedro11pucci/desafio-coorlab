542bdc9d24ac939271e407b980306e8a41c89497
pedro11pucci <pedro11pucci@gmail.com>
2024-03-23 16:00:55 -0300

    Add Flask-Cors to requirements.txt

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Implemente aqui o script para executar a sua solução"
+cd frontend
+npm install
+npm run dev &
+
+cd ..
+
+cd backend
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements
+python app.py
